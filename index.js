53e53907fe59aa8b2cf5493b3c024b2825c2cae4
Santhosh-SNTS <networksnts@gmail.com>
2022-03-11 07:12:42 +0000

    fourth commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello this is santhosh- jenkins reuse-1')
+  response.send('Hello this is santhosh- jenkins reuse-2')
 })
 
 app.listen(app.get('port'), function() {
